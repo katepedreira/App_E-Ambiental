6dc89b03ecb8ded005033f44a12b3ac6dd37ac5a
Carolina Campos <carolinacampos.dev@gmail.com>
2023-11-25 17:58:17 -0300

    Adicionado Navigator

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet } from 'react-native';
+import Navigator from './src/view/components/navigator';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+    <Navigator/>
+     
   );
+
 }
 
 const styles = StyleSheet.create({
