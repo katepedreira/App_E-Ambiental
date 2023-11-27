13218be21844f2e986af55543833672ce4fad771
Carolina Campos <carolinacampos.dev@gmail.com>
2023-11-27 20:02:33 -0300

    Adicionado Navegação do Menu

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreenView from '../view/WelcomeScreenView/WelcomoeScreenView';
 import LoginView from '../view/LoginView/LoginView';
 import TipoResiduoView from '../view/TipoResiduoView/TipoResiduoView';
+import MenuView from '../view/MenuView/MenuView';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const Navigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Menu" component={MenuView} />
         <Stack.Screen name="Welcome" component={WelcomeScreenView} />
         <Stack.Screen name="Login" component={LoginView} />
         <Stack.Screen name="TipoResiduo" component={TipoResiduoView}/>
